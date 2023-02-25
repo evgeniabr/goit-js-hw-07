854aedb24407d8cc80cc6de8ad66dde8969ec69a
evgeniabr <113677474+evgeniabr@users.noreply.github.com>
2023-02-25 18:20:23 +0200

    **

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,19 +1,16 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 const galleryContainer = document.querySelector(".gallery");
 console.log(galleryContainer);
 const galleryMarkup = createGalleryMarkup(galleryItems);
 
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
 
-
-
-galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
-
-function createGalleryMarkup (items){
-    
-    return galleryItems.map(({ preview, original, description }) => {
-        return ` <a class="gallery__link" href= "${original}">
+function createGalleryMarkup(items) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return ` <a class="gallery__link" href= "${original}">
     <img
       class= "gallery__image"
       src= "${preview}"
@@ -21,28 +18,54 @@ function createGalleryMarkup (items){
       alt= "${description}"
     />
   </a>`;
-    }).join('');
+    })
+    .join("");
 }
-
 
 console.log(galleryItems);
 
+galleryContainer.addEventListener("click", onGalleryContainerClick);
 
-
- galleryContainer.addEventListener('clikc', onGalleryContainerClikc);
-
-function onGalleryContainerClikc(event) {
+function onGalleryContainerClick(event) {
   event.preventDefault();
 
   if (event.target.nodeName !== "IMG") {
     return;
-  };
+  }
 
   const turgetSource = event.target.dataset.source;
-  const instance = basicLightbox.create(`
-   <img src="${turgetSource}" width="800" height="600">
-`);
 
- instance.show();
+  const instance = basicLightbox.create(
+    `
+   <img src="${turgetSource}" width="800" height="600">
+`,
+    {
+      onShow: (instance) => {
+        window.addEventListener("keydown", onEskKeyPress);
+      },
+      onClose: (instance) => {
+        window.removeEventListener("keydown", onEskKeyPress);
+      },
+    }
+  );
+  function onEskKeyPress(event) {
+    const ESK_KEY_CODE = "Escape";
+    if (event.code === ESK_KEY_CODE) {
+      instance.close();
+    }
+  }
+
+  console.log(instance);
+  instance.show();
 }
 
+// function onOpenModal() {
+//   if (instance.show())
+//     window.addEventListener('keydown', onEskKeyPress);
+// }
+
+// function onCloseModal() {
+//   if (instance.close()) {
+//     window.removeEventListener('keydown', onEskKeyPress);
+//   };
+// }
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryList = document.querySelector(".gallery");
+console.log(galleryList);
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryList.insertAdjacentHTML("beforeend", galleryMarkup);
+
+function createGalleryMarkup(items) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return ` <a class="gallery__link" href= "${original}">
+    <img
+      class= "gallery__image"
+      src= "${preview}"
+      alt= "${description}"
+    />
+  </a>`;
+    })
+    .join("");
+}
+
+
+
+
+    const lightbox = new SimpleLightbox('.gallery__link', {captionsData:'alt', captionDelay: 250 });
+
 console.log(galleryItems);
